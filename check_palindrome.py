6e8d13a378eef4f3686ca5789c58157a4407492f
swetha Narni <ysswetha@memphis.edu>
2024-02-07 15:32:23 -0600

    Merged file-input into main, added choice for file vs text

--- check_palindrome.py
+++ check_palindrome.py
@@ -3,9 +3,16 @@ def check_palindrome(text):
         print(f"{text} is a palindrome")
     else:
         print(f"{text} is not a palindrome")
+choice = input("Enter 't' for text or 'f' for file: ")
+if choice == "t":
+    text = input("Enter text to check for palindrome: ")
+    check_palindrome(text)
+elif choice == "f":
     try:
         file = input("Enter filepath for palindrome check: ")
         text = open(file, "r").read()
         check_palindrome(text)
-    except(OSError):
+    except(OSError):        
         print(f"Unable to process file at {file}")
+else:
+    print("Invalid choice")
